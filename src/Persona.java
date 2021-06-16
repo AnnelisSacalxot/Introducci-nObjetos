b50fcd5f009eaab08d460fdff9feb820f03dcad8
Annelís <juanysacalxot376@gmail.com>
2021-06-15 22:43:27 -0600

    Creación de los 5 métodos que se solicitan

--- src/Persona.java
+++ src/Persona.java
@@ -1,5 +1,51 @@
-public class App {
+public class Persona {
+
+    //Se colocan los atributos y se encapsulan porque no interesa
+    // que otros puedan acceder a estos atributos
+
+    private String nombre;
+    private byte edad;
+    private byte dni;
+    private char sexo;
+    private short peso;
+    private short altura;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Creación del objeto
+        Persona persona = new Persona();
+
+    }
+
+    //Se inicializa el constructor con todos los atributos de la clase
+    public Persona(){
+     this.nombre = nombre;  
+     this.edad = edad;
+     this.dni = dni;
+     this.sexo = sexo;
+     this.peso = peso;
+     this.altura = altura;
+    }
+
+    //Creación de métodos
+
+    public void calcularIMC(){
+
+    }
+
+    public void mayorEdad(){
+
+    }
+
+    public void comprobarSexo(){
+
+    }
+
+    public void generaDNI(){
+
+    }
+
+    //se creara el metodo toString
+    public String toString(){
+        return "Nombre"+nombre;
     }
 }
